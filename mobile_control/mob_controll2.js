825e8a72d0c69b3d42bb04ccef7b5b36065998e5
jooam <ja960508@naver.com>
2021-07-21 11:58:12 +0900

    overwrite

--- mobile_control/mob_controll2.js
+++ mobile_control/mob_controll2.js
@@ -1,3 +1,4 @@
+
 const iframe_cotents = document.getElementById("img_collection");
 const content_box = document.getElementsByClassName("content_box")[0];
 
@@ -5,10 +6,14 @@ iframe_cotents.style.height = `${window.innerHeight - 199}px`;
 content_box.style.height = `${window.innerHeight - 199}px`;
 
 window.onload = function () {
-  iframe_cotents.contentWindow.postMessage(window.innerHeight, "*");
+  //iframe_cotents.contentWindow.postMessage(window.innerHeight, "*");
+  document.getElementById("img_collection").contentWindow.adjust_size(window.innerHeight);
 };
 window.addEventListener("resize", () => {
+  /*
   iframe_cotents.style.height = `${window.innerHeight - 199}px`;
   content_box.style.height = `${window.innerHeight - 199}px`;
   iframe_cotents.contentWindow.postMessage(window.innerHeight, "*");
+  */
+  document.getElementById("img_collection").contentWindow.adjust_size(window.innerHeight);
 });
