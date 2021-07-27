a7e2467b11e8a36035535e27499995f456c68197
jooam <ja960508@naver.com>
2021-07-27 10:07:18 +0900

    change variable type

--- mobile_control/mob_controll2.js
+++ mobile_control/mob_controll2.js
@@ -1,7 +1,9 @@
-const img_collection = document.getElementById("img_collection");
-const attendance_collection = document.getElementById("attendance_collection");
-const presentation_collection = document.getElementById("presentation_collection");
-const content_box = document.getElementsByClassName("content_box")[0];
+var img_collection = document.getElementById('img_collection');
+var attendance_collection = document.getElementById('attendance_collection');
+var presentation_collection = document.getElementById(
+  'presentation_collection'
+);
+var content_box = document.getElementsByClassName('content_box')[0];
 
 img_collection.style.height = `${window.innerHeight - 199}px`;
 attendance_collection.style.height = `${window.innerHeight - 199}px`;
@@ -13,7 +15,7 @@ window.onload = function () {
   attendance_collection.contentWindow.adjust_size(window.innerHeight);
   presentation_collection.contentWindow.adjust_size(window.innerHeight);
 };
-window.addEventListener("resize", () => {
+window.addEventListener('resize', () => {
   img_collection.contentWindow.adjust_size(window.innerHeight);
   attendance_collection.contentWindow.adjust_size(window.innerHeight);
   presentation_collection.contentWindow.adjust_size(window.innerHeight);
